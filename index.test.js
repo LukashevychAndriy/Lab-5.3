abc33dbb7fab35cc290e88714e3c7e55bb025a04
LukashevychAndriy <andriy.lukashewich@gmail.com>
2021-10-20 22:23:55 +0300

    changed unit-tests

--- index.test.js
+++ index.test.js
@@ -1,14 +1,8 @@
 const { expect } = require('@jest/globals');
 const { f } = require('./functions');
 
-test('', () => {
+test('f should return value based on x argument', () => {
   expect(f(100)).toBe(1.386795555294441);
-});
-
-test('', () => {
   expect(f(0)).toBe(0);
-});
-
-test('', () => {
   expect(f(-9)).toBe(-0.44427135565772247);
 });
